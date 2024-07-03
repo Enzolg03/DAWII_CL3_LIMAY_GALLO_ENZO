07889a7931a65c20c9ec7915c8d50c876bec1670
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-07-03 18:35:37 -0500

    Pregunta 03 - agregando logica al service y comment-list.component.ts y .html

--- src/app/components/dashboard/comments/comment-list/comment-list.component.ts
+++ src/app/components/dashboard/comments/comment-list/comment-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommentService } from '../comment.service';
+import { Comment } from '../comment';
+import { MaterialModule } from '../../../../angular-material/material/material.module';
 
 @Component({
   selector: 'app-comment-list',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './comment-list.component.html',
   styleUrl: './comment-list.component.css'
 })
-export class CommentListComponent {
+export class CommentListComponent implements OnInit{
+  displayedColumns: string[] = ['postId', 'id', 'name', 'email','body'];
+  comments: Comment[] = [];
 
+  constructor(private commentService : CommentService){}
+  
+  ngOnInit(): void {
+    this.commentService.getAllComments().subscribe((data) => {
+      this.comments = this.filterCommentsByDomain(data, 'garfield.biz');
+    });
+  }
+  private filterCommentsByDomain(comments: Comment[], domain: string): Comment[] {
+    return comments.filter(comment => comment.email.endsWith(`@${domain}`));
+  }
 }
--- src/app/components/dashboard/comments/comment.service.ts
+++ src/app/components/dashboard/comments/comment.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comment } from './comment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllComments(): Observable<Comment[]> {
+    return this.httpClient.get<Comment[]>('https://jsonplaceholder.typicode.com/comments');
+  }
 }
