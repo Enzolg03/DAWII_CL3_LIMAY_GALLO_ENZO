87ed8433363a1793daddeb3214faa7859b92c8b2
Enzolg03 <enzojoaquinlimaygallo@gmail.com>
2024-07-03 17:59:48 -0500

    Pregunta 01 - agregando angular-material y menu con 2 opciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CharacterListComponent } from './components/dashboard/characters/character-list/character-list.component';
+import { CommentListComponent } from './components/dashboard/comments/comment-list/comment-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: DashboardComponent,
+        children: [
+            { path: "character", component: CharacterListComponent},
+            { path: "comment", component: CommentListComponent}
+        ]
+    },
+];
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MaterialModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  constructor(private router: Router, 
+    private route: ActivatedRoute){}
+    irHome(){
+      this.router.navigate([""],{relativeTo: this.route})
+    }
+    irCharacter(){
+      this.router.navigate(["character"],{relativeTo: this.route})
+    }
+    irComment(){
+      this.router.navigate(["comment"],{relativeTo: this.route})
+    }
 }
